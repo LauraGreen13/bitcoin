e30627ba4aad7f96104d6242fa3f6dd389883e40
laura <laura.ionel13@gmail.com>
2018-04-24 14:34:26 +0200

    fix conflicts

---
+++ _benchmarks/plots_avg_time.py
@@ -0,0 +1,131 @@
+
+from pylab import figure, show, legend, ylabel
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import matplotlib.ticker as plticker
+
+
+# create the general figure
+# fig = figure(figsize=(10,10))
+
+# and the first axes using subplot populated with data
+# ax1 = fig.add_subplot(111)
+
+#Benchmark,count,min,max,average,min_cycles,max_cycles,average_cycles
+# number of transactions: 2
+# UTXO per transaction: 2
+# CCoinsCaching,180224,0.000005889713066,0.000006434565876,0.000005953029772,15265,16678,15430
+# coins cache size: 4
+
+
+input=sys.argv[1]
+output=sys.argv[2]
+
+f1 = open(input)
+#f2 = open('tmp2_0')
+
+lines = f1.read().splitlines()
+#lines = lines[25:]
+
+
+tx_no = lines[1].split(":")[-1]
+utxo_list = []
+times_list = []
+cache_size_list = []
+info_list = []
+
+print("tx_no: %s" % tx_no)
+
+
+for i in range(2, len(lines) - 3, 4):
+    #print(lines[i].split(":"))
+    utxo_no = lines[i].split(":")[-1]
+    cpu_cycles = lines[i+1].split(",")[-4]
+    #print(cpu_cycles)
+    cache_size = lines[i+2].split(":")[-1]
+    #print("utxo_no: %s, cpu_cycles: %s, cache_size: %s" % (utxo_no, cpu_cycles, cache_size))
+    utxo_list.append(int(utxo_no))
+    times_list.append(float(cpu_cycles))
+    cache_size_list.append(cache_size)
+    info_list.append(utxo_no + " utxo\n" + cache_size + " B")
+
+
+print("END")
+
+# line1 = ax1.plot(utxo_list, 'o-')
+# # ylabel("-dbcache=300 (default)")
+#
+# # now, the second axes that shares the x-axis with the ax1
+# ax2 = fig1.add_subplot(111, sharex=ax1, frameon=False)
+# line2 = ax2.plot(cache_size_list, 'xr-')
+# ax2.yaxis.tick_right()
+# ax2.yaxis.set_label_position("right")
+# # ylabel("-dbcache=5")
+#
+# # y_line =
+#
+# # for the legend, remember that we used two different axes so, we need
+# # to build the legend manually
+# legend((line1, line2), ("1", "2"))
+# show()
+#
+
+
+#x = times_list
+#y = utxo_list
+
+x = utxo_list
+y = times_list
+
+print (x)
+print (y)
+print (info_list)
+
+# fig = plt.figure()
+# fig,ax = plt.subplots()
+
+fig = plt.figure(figsize=(15,15))
+ax = plt.subplot(111)
+
+#x=x[1:800]
+#y=y[1:800]
+
+#ax.set_ylim([0.000003,0.000005])
+ax.plot(x,y)
+ax.scatter(x,y)
+
+'''
+info_indices = np.arange(0, max(y), 40)
+
+ax.set_ylim(10,max(y))
+
+ax.set_yticks(info_indices) # choose which x locations to have ticks
+ax.set_yticklabels(info_list) #
+'''
+
+#the same but for the other axis
+'''
+info_indices = np.arange(0, max(x), 40)
+
+ax.set_ylim(10,max(x))
+
+ax.set_xticks(info_indices) # choose which x locations to have ticks
+ax.set_xticklabels(info_list) #
+'''
+
+
+#ax.set_yticks(info_list)
+#plt.yticks(info_list)
+
+
+plt.xlabel("CPU cycles")
+plt.ylabel("utxo number\ncache size")
+plt.title("NO Cache Flush")
+# plt.legend((x, y), ("cpu cycles", "UTXO number\ncache size"))
+
+# ax1.set_ylabel('UTXO number\nCache size')
+
+fig.savefig(output)
+
+
--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -8,118 +8,224 @@
 #include "util.h"
 #include "random.h"
 
-#include <leveldb/cache.h>
-#include <leveldb/env.h>
-#include <leveldb/filter_policy.h>
+//#include <leveldb/cache.h>
+//#include <leveldb/env.h>
+//#include <leveldb/filter_policy.h>
 #include <memenv.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <algorithm>
 
-class CBitcoinLevelDBLogger : public leveldb::Logger {
-public:
-    // This code is adapted from posix_logger.h, which is why it is using vsprintf.
-    // Please do not do this in normal code
-    virtual void Logv(const char * format, va_list ap) override {
-            if (!LogAcceptCategory(BCLog::LEVELDB)) {
-                return;
-            }
-            char buffer[500];
-            for (int iter = 0; iter < 2; iter++) {
-                char* base;
-                int bufsize;
-                if (iter == 0) {
-                    bufsize = sizeof(buffer);
-                    base = buffer;
-                }
-                else {
-                    bufsize = 30000;
-                    base = new char[bufsize];
-                }
-                char* p = base;
-                char* limit = base + bufsize;
+//class CBitcoinLevelDBLogger : public leveldb::Logger {
+//public:
+//    // This code is adapted from posix_logger.h, which is why it is using vsprintf.
+//    // Please do not do this in normal code
+//    virtual void Logv(const char * format, va_list ap) override {
+//            if (!LogAcceptCategory(BCLog::LEVELDB)) {
+//                return;
+//            }
+//            char buffer[500];
+//            for (int iter = 0; iter < 2; iter++) {
+//                char* base;
+//                int bufsize;
+//                if (iter == 0) {
+//                    bufsize = sizeof(buffer);
+//                    base = buffer;
+//                }
+//                else {
+//                    bufsize = 30000;
+//                    base = new char[bufsize];
+//                }
+//                char* p = base;
+//                char* limit = base + bufsize;
+//
+//                // Print the message
+//                if (p < limit) {
+//                    va_list backup_ap;
+//                    va_copy(backup_ap, ap);
+//                    // Do not use vsnprintf elsewhere in bitcoin source code, see above.
+//                    p += vsnprintf(p, limit - p, format, backup_ap);
+//                    va_end(backup_ap);
+//                }
+//
+//                // Truncate to available space if necessary
+//                if (p >= limit) {
+//                    if (iter == 0) {
+//                        continue;       // Try again with larger buffer
+//                    }
+//                    else {
+//                        p = limit - 1;
+//                    }
+//                }
+//
+//                // Add newline if necessary
+//                if (p == base || p[-1] != '\n') {
+//                    *p++ = '\n';
+//                }
+//
+//                assert(p <= limit);
+//                base[std::min(bufsize - 1, (int)(p - base))] = '\0';
+//                LogPrintStr(base);
+//                if (base != buffer) {
+//                    delete[] base;
+//                }
+//                break;
+//            }
+//    }
+//};
 
-                // Print the message
-                if (p < limit) {
-                    va_list backup_ap;
-                    va_copy(backup_ap, ap);
-                    // Do not use vsnprintf elsewhere in bitcoin source code, see above.
-                    p += vsnprintf(p, limit - p, format, backup_ap);
-                    va_end(backup_ap);
-                }
 
-                // Truncate to available space if necessary
-                if (p >= limit) {
-                    if (iter == 0) {
-                        continue;       // Try again with larger buffer
-                    }
-                    else {
-                        p = limit - 1;
-                    }
-                }
-
-                // Add newline if necessary
-                if (p == base || p[-1] != '\n') {
-                    *p++ = '\n';
-                }
-
-                assert(p <= limit);
-                base[std::min(bufsize - 1, (int)(p - base))] = '\0';
-                LogPrintStr(base);
-                if (base != buffer) {
-                    delete[] base;
-                }
-                break;
-            }
-    }
+//sqlite github version
+static const char *sql_db_init[] = {
+    "CREATE TABLE tab(k BLOB PRIMARY KEY, v BLOB) WITHOUT ROWID",
 };
 
-static leveldb::Options GetOptions(size_t nCacheSize)
-{
-    leveldb::Options options;
-    options.block_cache = leveldb::NewLRUCache(nCacheSize / 2);
-    options.write_buffer_size = nCacheSize / 4; // up to two write buffers may be held in memory simultaneously
-    options.filter_policy = leveldb::NewBloomFilterPolicy(10);
-    options.compression = leveldb::kNoCompression;
-    options.max_open_files = 64;
-    options.info_log = new CBitcoinLevelDBLogger();
-    if (leveldb::kMajorVersion > 1 || (leveldb::kMajorVersion == 1 && leveldb::kMinorVersion >= 16)) {
-        // LevelDB versions before 1.16 consider short writes to be corruption. Only trigger error
-        // on corruption in later versions.
-        options.paranoid_checks = true;
-    }
-    return options;
-}
+
+
+
+
+static const char *sql_db_configure[] = {
+//    "PRAGMA page_size=4096",
+//	"PRAGMA cache_size=-4000",    // max cache size; negative = # of kibibytes
+//	"PRAGMA journal_mode=WAL",
+//	"PRAGMA wal_autocheckpoint=10000",
+//
+	"PRAGMA page_size = 4096",
+	"PRAGMA cache_size=10000",
+//	"PRAGMA locking_mode=EXCLUSIVE",
+	"PRAGMA journal_mode=WAL"
+	"PRAGMA SQLITE_DEFAULT_CACHE_SIZE=5000",
+	"PRAGMA schema.journal_size_limit = 1048576"
+
+};
+
+static const char *sql_stmt_text[] = {
+    "REPLACE INTO tab(k, v) VALUES(:k, :v)",            // DBW_PUT
+    "SELECT v FROM tab WHERE k = :k",                   // DBW_GET
+    "DELETE FROM tab WHERE k = :k",                     // DBW_DELETE
+    "BEGIN TRANSACTION",                                // DBW_BEGIN
+    "COMMIT TRANSACTION",                               // DBW_COMMIT
+    "SELECT COUNT(*) FROM tab",                         // DBW_GET_ALL_COUNT
+    "SELECT k, v FROM tab WHERE k >= :k ORDER BY k",    // DBW_SEEK_SORTED
+};
+
+
+//static leveldb::Options GetOptions(size_t nCacheSize)
+//{
+//    leveldb::Options options;
+//    options.block_cache = leveldb::NewLRUCache(nCacheSize / 2);
+//    options.write_buffer_size = nCacheSize / 4; // up to two write buffers may be held in memory simultaneously
+//    options.filter_policy = leveldb::NewBloomFilterPolicy(10);
+//    options.compression = leveldb::kNoCompression;
+//    options.max_open_files = 64;
+//    options.info_log = new CBitcoinLevelDBLogger();
+//    if (leveldb::kMajorVersion > 1 || (leveldb::kMajorVersion == 1 && leveldb::kMinorVersion >= 16)) {
+//        // LevelDB versions before 1.16 consider short writes to be corruption. Only trigger error
+//        // on corruption in later versions.
+//        options.paranoid_checks = true;
+//    }
+//    return options;
+//}
 
 CDBWrapper::CDBWrapper(const fs::path& path, size_t nCacheSize, bool fMemory, bool fWipe, bool obfuscate)
 {
-    penv = nullptr;
-    readoptions.verify_checksums = true;
-    iteroptions.verify_checksums = true;
-    iteroptions.fill_cache = false;
-    syncoptions.sync = true;
-    options = GetOptions(nCacheSize);
-    options.create_if_missing = true;
+//    penv = nullptr;
+//    readoptions.verify_checksums = true;
+//    iteroptions.verify_checksums = true;
+//    iteroptions.fill_cache = false;
+//    syncoptions.sync = true;
+//    options = GetOptions(nCacheSize);
+//    options.create_if_missing = true;
+
+	std::string fp;
+	const char *filename = NULL;
+	bool need_init = false;
+
     if (fMemory) {
-        penv = leveldb::NewMemEnv(leveldb::Env::Default());
-        options.env = penv;
+//        penv = leveldb::NewMemEnv(leveldb::Env::Default());
+//        options.env = penv;
+    	filename = ":memory:";
+		need_init = true;
     } else {
+    	fp = path.string();
+		fp = fp + "/db";
         if (fWipe) {
-            LogPrintf("Wiping LevelDB in %s\n", path.string());
-            leveldb::Status result = leveldb::DestroyDB(path.string(), options);
-            dbwrapper_private::HandleError(result);
+//            LogPrintf("Wiping LevelDB in %s\n", path.string());
+//            leveldb::Status result = leveldb::DestroyDB(path.string(), options);
+//            dbwrapper_private::HandleError(result);
+            LogPrintf("Wiping DB %s\n", fp);
+
+            std::cout << "Wiping DB %s\n" + fp + "\n";
+            fs::remove(fp);
         }
+//        TryCreateDirectories(path);
+//        LogPrintf("Opening LevelDB in %s\n", path.string());
         TryCreateDirectories(path);
-        LogPrintf("Opening LevelDB in %s\n", path.string());
+		LogPrintf("Opening DB in ", path.string());
+//		std::cout << "Opening DB in " + path.string() + "\n";
+
+		filename = fp.c_str();
     }
-    leveldb::Status status = leveldb::DB::Open(options, path.string(), &pdb);
-    dbwrapper_private::HandleError(status);
-    LogPrintf("Opened LevelDB successfully\n");
+
+//    std::cout << "Opening DB in " + path.string() + "\n";
+
+//    leveldb::Status status = leveldb::DB::Open(options, path.string(), &pdb);
+//    dbwrapper_private::HandleError(status);
+//    LogPrintf("Opened LevelDB successfully\n");
+//
+    // open existing sqlite db
+	int orc = sqlite3_open_v2(filename, &psql, SQLITE_OPEN_READWRITE | SQLITE_OPEN_NOMUTEX, NULL);
+
+	// if open-existing failed, attempt to create new db
+	if (orc != SQLITE_OK) {
+		orc = sqlite3_open_v2(filename, &psql, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX, NULL);
+		if (orc != SQLITE_OK)
+			throw(dbwrapper_error("DB open failed, err " + itostr(orc)));
+
+		need_init = true;
+	}
 
     if (gArgs.GetBoolArg("-forcecompactdb", false)) {
-        LogPrintf("Starting database compaction of %s\n", path.string());
-        pdb->CompactRange(nullptr, nullptr);
-        LogPrintf("Finished database compaction of %s\n", path.string());
+//        LogPrintf("Starting database compaction of %s\n", path.string());
+//        pdb->CompactRange(nullptr, nullptr);
+//        LogPrintf("Finished database compaction of %s\n", path.string());
+
+    	//TODO: do stuff here
     }
+
+    // configure database params
+	if (need_init) {
+		for (unsigned int i = 0; i < ARRAYLEN(sql_db_init); i++)
+			if (sqlite3_exec(psql, sql_db_configure[i], NULL, NULL, NULL) != SQLITE_OK)
+				throw(dbwrapper_error("DB configuration failed, stmt " + itostr(i)));
+	}
+
+	// first time: initialize new db schema
+	if (need_init) {
+		for (unsigned int i = 0; i < ARRAYLEN(sql_db_init); i++)
+			if (sqlite3_exec(psql, sql_db_init[i], NULL, NULL, NULL) != SQLITE_OK)
+				throw(dbwrapper_error("DB one-time setup failed, stmt " + itostr(i)));
+	}
+
+	// pre-compile SQL statements
+	for (unsigned sqlidx = 0; sqlidx < ARRAYLEN(sql_stmt_text); sqlidx++) {
+		sqlite3_stmt *pstmt = NULL;
+
+		int src = sqlite3_prepare_v2(psql,
+							   sql_stmt_text[sqlidx],
+							   strlen(sql_stmt_text[sqlidx]),
+							   &pstmt, NULL);
+		if (src != SQLITE_OK)
+			throw(dbwrapper_error("DB compile failed, err " + itostr(src) + ": " + std::string(sql_stmt_text[sqlidx])));
+
+		stmts.push_back(pstmt);
+	}
+
+
+	LogPrintf("Opened DB successfully\n");
+//	std::cout << "Opened DB successfully\n";
+
+
 
     // The base-case obfuscation key, which is a noop.
     obfuscate_key = std::vector<unsigned char>(OBFUSCATE_KEY_NUM_BYTES, '\000');
@@ -143,23 +249,92 @@ CDBWrapper::CDBWrapper(const fs::path& path, size_t nCacheSize, bool fMemory, bo
 
 CDBWrapper::~CDBWrapper()
 {
-    delete pdb;
-    pdb = nullptr;
-    delete options.filter_policy;
-    options.filter_policy = nullptr;
-    delete options.info_log;
-    options.info_log = nullptr;
-    delete options.block_cache;
-    options.block_cache = nullptr;
-    delete penv;
-    options.env = nullptr;
+//    delete pdb;
+//    pdb = nullptr;
+//    delete options.filter_policy;
+//    options.filter_policy = nullptr;
+//    delete options.info_log;
+//    options.info_log = nullptr;
+//    delete options.block_cache;
+//    options.block_cache = nullptr;
+//    delete penv;
+//    options.env = nullptr;
+
+
+
+	for (unsigned int i = 0; i < stmts.size(); i++) {
+		sqlite3_finalize(stmts[i]);
+	}
+
+	stmts.clear();
+
+	sqlite3_close(psql);
+	psql = NULL;
 }
 
 bool CDBWrapper::WriteBatch(CDBBatch& batch, bool fSync)
 {
-    leveldb::Status status = pdb->Write(fSync ? syncoptions : writeoptions, &batch.batch);
-    dbwrapper_private::HandleError(status);
-    return true;
+//    leveldb::Status status = pdb->Write(fSync ? syncoptions : writeoptions, &batch.batch);
+//    dbwrapper_private::HandleError(status);
+//    return true;
+
+//	std::cout<< "batch size:" << batch.entries.size();
+	// begin DB transaction
+	int src = sqlite3_step(stmts[DBW_BEGIN]);
+	int rrc = sqlite3_reset(stmts[DBW_BEGIN]);
+
+	if ((src != SQLITE_DONE) || (rrc != SQLITE_OK))
+		throw dbwrapper_error("DB cannot begin transaction, err " + itostr(src) + "," + itostr(rrc));
+
+	int bytes_written = 0;
+	for (unsigned int wr = 0; wr < batch.entries.size(); wr++) {
+		BatchEntry& be = batch.entries[wr];
+		bytes_written += sizeof(be.key);
+		bytes_written += sizeof(be.value);
+
+		sqlite3_stmt *stmt = NULL;
+
+		// get the SQL stmt to exec
+		if (be.isErase)
+			stmt = stmts[DBW_DELETE];
+		else
+			stmt = stmts[DBW_PUT];
+
+		// bind column 0: key
+		if (sqlite3_bind_blob(stmt, 1, be.key.c_str(), be.key.size(),
+							  SQLITE_TRANSIENT) != SQLITE_OK)
+			throw dbwrapper_error("DB cannot bind blob key");
+
+		// if inserting, bind column 1: value
+		if (!be.isErase)
+			if (sqlite3_bind_blob(stmt, 2, be.value.c_str(), be.value.size(),
+								  SQLITE_TRANSIENT) != SQLITE_OK)
+				throw dbwrapper_error("DB cannot bind blob value");
+
+		// exec INSERT/DELETE
+		src = sqlite3_step(stmt);
+		rrc = sqlite3_reset(stmt);
+
+		if ((src != SQLITE_DONE) || (rrc != SQLITE_OK)) {
+			std::cout << itostr(src) + "," + itostr(rrc);
+			throw dbwrapper_error("DB failed update, err " + itostr(src) + "," + itostr(rrc));
+		}
+
+	}
+
+	src = sqlite3_step(stmts[DBW_COMMIT]);
+	rrc = sqlite3_reset(stmts[DBW_COMMIT]);
+
+
+	std::cout << "size of write batch is: " << bytes_written << "\n";
+	if ((src != SQLITE_DONE) || (rrc != SQLITE_OK)) {
+		std::cout << itostr(src) + "," + itostr(rrc);
+		throw dbwrapper_error("DB cannot commit transaction, err" + itostr(src) + "," + itostr(rrc));
+	}
+
+	batch.Clear();
+
+	return true;
 }
 
 // Prefixed with null character to avoid collisions with other keys
@@ -184,31 +359,47 @@ std::vector<unsigned char> CDBWrapper::CreateObfuscateKey() const
 
 bool CDBWrapper::IsEmpty()
 {
-    std::unique_ptr<CDBIterator> it(NewIterator());
-    it->SeekToFirst();
-    return !(it->Valid());
+//    std::unique_ptr<CDBIterator> it(NewIterator());
+//    it->SeekToFirst();
+//    return !(it->Valid());
+
+	int src = sqlite3_step(stmts[DBW_GET_ALL_COUNT]);
+	if (src != SQLITE_ROW) {
+		sqlite3_reset(stmts[DBW_GET_ALL_COUNT]);
+		throw dbwrapper_error("DB read count failure");
+	}
+
+	int64_t count = sqlite3_column_int64(stmts[DBW_GET_ALL_COUNT], 0);
+	sqlite3_reset(stmts[DBW_GET_ALL_COUNT]);
+
+	return (count == 0);
+
 }
 
-CDBIterator::~CDBIterator() { delete piter; }
-bool CDBIterator::Valid() { return piter->Valid(); }
-void CDBIterator::SeekToFirst() { piter->SeekToFirst(); }
-void CDBIterator::Next() { piter->Next(); }
+
+CDBIterator::~CDBIterator() {
+	sqlite3_reset(pstmt);
+}
+
+//TODO: empty impl to compile: this won't be needed
+bool CDBIterator::Valid() { return valid; }
+void CDBIterator::SeekToFirst() {}
 
 namespace dbwrapper_private {
 
-void HandleError(const leveldb::Status& status)
-{
-    if (status.ok())
-        return;
-    LogPrintf("%s\n", status.ToString());
-    if (status.IsCorruption())
-        throw dbwrapper_error("Database corrupted");
-    if (status.IsIOError())
-        throw dbwrapper_error("Database I/O error");
-    if (status.IsNotFound())
-        throw dbwrapper_error("Database entry missing");
-    throw dbwrapper_error("Unknown database error");
-}
+//void HandleError(const leveldb::Status& status)
+//{
+//    if (status.ok())
+//        return;
+//    LogPrintf("%s\n", status.ToString());
+//    if (status.IsCorruption())
+//        throw dbwrapper_error("Database corrupted");
+//    if (status.IsIOError())
+//        throw dbwrapper_error("Database I/O error");
+//    if (status.IsNotFound())
+//        throw dbwrapper_error("Database entry missing");
+//    throw dbwrapper_error("Unknown database error");
+//}
 
 const std::vector<unsigned char>& GetObfuscateKey(const CDBWrapper &w)
 {
